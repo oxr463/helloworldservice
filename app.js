e73b851c8b63cb0c2adf0b0e7afebf45aed589cd
Lucas Ramage <oxr463@gmx.us>
2020-01-30 18:23:38 -0500

    Update app.js
    Bug: https://github.com/oxr463/helloworldservice/issues/6

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const port = process.env.PORT;
+
 var express = require('express');
 
 var helloworldservice = require('./helloworldservice');
 var app = express();
 
 app.use('/helloworldservice',helloworldservice);
-app.listen(3000);
+app.listen(port);
 
-console.log('server running on port 3000');
+console.log('server running on port ' + port);
