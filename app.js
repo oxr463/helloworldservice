584645148bbae0ca3e534a9432a7f5c7dcaaa851
Lucas Ramage <oxr463@gmx.us>
2020-03-05 08:14:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express');
 var helloworldservice = require('./helloworldservice');
 var app = express();
 
-app.use('/helloworldservice',helloworldservice);
+app.use('/helloworldservice', helloworldservice);
 app.listen(port);
 
 console.log('server running on port ' + port);
