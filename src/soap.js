f69fd0ee461be50f89f00c850b36d2c639522796
Lucas Ramage <ramage.lucas@protonmail.com>
2020-04-28 14:01:03 -0400

    Use relative path for wsdl.xml

--- src/soap.js
+++ src/soap.js
@@ -1,26 +1,26 @@
-const http = require('http');
-const soap = require('soap');
+const http = require("http");
+const soap = require("soap");
 
 const PORT = 3000;
 
 var helloworldservice = {
-    Hello_Service: {
-        Hello_Port: {
-            sayHello: function (args, callback) {
-                console.log('sayHello: '+JSON.stringify(args));
-                callback({'greeting': 'Hello '+args.firstName.$value});
-            }
-        }
-    }
+  Hello_Service: {
+    Hello_Port: {
+      sayHello: function (args, callback) {
+        console.log("sayHello: " + JSON.stringify(args));
+        callback({ greeting: "Hello " + args.firstName.$value });
+      },
+    },
+  },
 };
 
-var wsdl = require('fs').readFileSync('wsdl.xml', 'utf8');
+var wsdl = require("fs").readFileSync("./wsdl.xml", "utf8");
 
 var server = http.createServer(function (request, response) {
-    response.end("404: Not Found: " + request.url);
+  response.end("404: Not Found: " + request.url);
 });
 
 server.listen(PORT);
-console.log('server running on port ' + PORT);
+console.log("server running on port " + PORT);
 
-soap.listen(server, '/helloworldservice', helloworldservice, wsdl);
+soap.listen(server, "/helloworldservice", helloworldservice, wsdl);
--- src/wsdl.js
+++ src/wsdl.js
@@ -1,116 +1,130 @@
-const async = require('async');
-const bodyParser = require('body-parser');
-const Builder = require('xml2js').Builder;
-const express = require('express');
+const async = require("async");
+const bodyParser = require("body-parser");
+const Builder = require("xml2js").Builder;
+const express = require("express");
 const fs = require("fs");
-const http = require('http');
-const parseString = require('xml2js').parseString;
+const http = require("http");
+const parseString = require("xml2js").parseString;
 const router = express.Router();
-const stripPrefix = require('xml2js').processors.stripPrefix;
+const stripPrefix = require("xml2js").processors.stripPrefix;
 const util = require("util");
-const url = require('url');
+const url = require("url");
 
-const utils = require('./utils');
+const utils = require("./utils");
 
-var wsdl = 'wsdl.xml';
+var wsdl = "./wsdl.xml";
 
-router.use(bodyParser.text({ type: '*/*' }));
+router.use(bodyParser.text({ type: "*/*" }));
 router.use(function timeLogStart(req, res, next) {
-    utils.logger('Request start');
-    res.locals.startTimeHR = process.hrtime();
-    next();
+  utils.logger("Request start");
+  res.locals.startTimeHR = process.hrtime();
+  next();
 });
 
 // http://localhost:3000/helloworldservice?wsdl
-router.get('/', function (req, res, next) {
-    utils.logger("GET");
-    if (req.query.wsdl === "") {
-        res.setHeader('Content-Type', 'application/xml');
-        res.statusCode = 200;
-        fs.readFile(wsdl, "utf8", function (err, data) {
-            if (err) {
-                endResponse(err);
-            } else {
-                endResponse(data);
-            }
-        });
-    } else {
-        endResponse("Invalid GET request");
-    }
+router.get("/", function (req, res, next) {
+  utils.logger("GET");
+  if (req.query.wsdl === "") {
+    res.setHeader("Content-Type", "application/xml");
+    res.statusCode = 200;
+    fs.readFile(wsdl, "utf8", function (err, data) {
+      if (err) {
+        endResponse(err);
+      } else {
+        endResponse(data);
+      }
+    });
+  } else {
+    endResponse("Invalid GET request");
+  }
 
-    function endResponse(data) {
-        res.write(data);
-        res.end();
-        next();
-    }
+  function endResponse(data) {
+    res.write(data);
+    res.end();
+    next();
+  }
 });
 
-router.post('/', function (req, res, next) {
-    utils.logger("POST");
-    async.waterfall([
-        function (cb) {
-            utils.logger('Convert POST request to usable JSON');
-            parseString(req.body, { tagNameProcessors: [stripPrefix] }, cb);
-        },
-        function (result, cb) {
-            utils.logger('Processing JSONized XML message');
-            var body = result["Envelope"]["Body"];
-            var sayHello = utils.search("sayHello", body);
-            var firstName = utils.search("firstName", sayHello);
-            var firstNameValue = utils.search("_", firstName);
-            cb(null, 'Hello ' + firstNameValue);
-        }
+router.post("/", function (req, res, next) {
+  utils.logger("POST");
+  async.waterfall(
+    [
+      function (cb) {
+        utils.logger("Convert POST request to usable JSON");
+        parseString(req.body, { tagNameProcessors: [stripPrefix] }, cb);
+      },
+      function (result, cb) {
+        utils.logger("Processing JSONized XML message");
+        var body = result["Envelope"]["Body"];
+        var sayHello = utils.search("sayHello", body);
+        var firstName = utils.search("firstName", sayHello);
+        var firstNameValue = utils.search("_", firstName);
+        cb(null, "Hello " + firstNameValue);
+      },
     ],
-        function (err, results) {
-            if (err) {
-                results = "I don't know your name";
-            }
-            var builder = new Builder();
+    function (err, results) {
+      if (err) {
+        results = "I don't know your name";
+      }
+      var builder = new Builder();
 
-            var jsonresponse = {
-                "soapenv:Envelope": {
-                    "$": {
-                        "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-                        "xmlns:xsd": "http://www.w3.org/2001/XMLSchema",
-                        "xmlns:soapenv": "http://schemas.xmlsoap.org/soap/envelope/",
-                        "xmlns:urn": "urn:examples:helloservice"
+      var jsonresponse = {
+        "soapenv:Envelope": {
+          $: {
+            "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+            "xmlns:xsd": "http://www.w3.org/2001/XMLSchema",
+            "xmlns:soapenv": "http://schemas.xmlsoap.org/soap/envelope/",
+            "xmlns:urn": "urn:examples:helloservice",
+          },
+          "soapenv:Header": [""],
+          "soapenv:Body": [
+            {
+              "urn:sayHelloResponse": [
+                {
+                  $: {
+                    "soapenv:encodingStyle":
+                      "http://schemas.xmlsoap.org/soap/encoding/",
+                  },
+                  greeting: [
+                    {
+                      _: results,
+                      $: {
+                        "xsi:type": "xsd:string",
+                      },
                     },
-                    "soapenv:Header": [""],
-                    "soapenv:Body": [{
-                        "urn:sayHelloResponse": [{
-                            "$": {
-                                "soapenv:encodingStyle": "http://schemas.xmlsoap.org/soap/encoding/"
-                            },
-                            "greeting": [{
-                                "_": results,
-                                "$": {
-                                    "xsi:type": "xsd:string"
-                                }
-                            }
-                            ]
-                        }
-                        ]
-                    }
-                    ]
-                }
-            }
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      };
 
-            var xmlresponse = builder.buildObject(jsonresponse);
+      var xmlresponse = builder.buildObject(jsonresponse);
 
-            utils.logger('Returning response Result: ' + JSON.stringify(results) + ' Error: ' + JSON.stringify(err));
+      utils.logger(
+        "Returning response Result: " +
+          JSON.stringify(results) +
+          " Error: " +
+          JSON.stringify(err)
+      );
 
-            res.setHeader('Content-Type', 'application/xml');
-            res.statusCode = 200;
-            res.end(xmlresponse);
-            next(err, results);
-        }
-    );
+      res.setHeader("Content-Type", "application/xml");
+      res.statusCode = 200;
+      res.end(xmlresponse);
+      next(err, results);
+    }
+  );
 });
 
 router.use(function timeLogEnd(req, res, next) {
-    var durationHR = process.hrtime(res.locals.startTimeHR);
-    utils.logger("Request end. Duration: %ds %dms", durationHR[0], durationHR[1] / 1000000);
-    next();
+  var durationHR = process.hrtime(res.locals.startTimeHR);
+  utils.logger(
+    "Request end. Duration: %ds %dms",
+    durationHR[0],
+    durationHR[1] / 1000000
+  );
+  next();
 });
 
 module.exports = router;
